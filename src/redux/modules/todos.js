030952b116b93db7f8af5f723e5aab022b7a9ad6
minseung-gang <strause1@nate.com>
2022-12-07 17:57:14 +0900

    상세보기 페이지 관련 리듀서및 액션객체 추가

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -44,28 +44,56 @@ const initialState = {
       content: "1",
       isDone: false,
     },
+    {
+      id: nanoid(),
+      order: 2,
+      title: "2",
+      content: "2",
+      isDone: false,
+    },
+    {
+      id: nanoid(),
+      order: 3,
+      title: "3",
+      content: "3",
+      isDone: false,
+    },
   ],
+  detail: [],
 };
 
 const todoList = (state = initialState, action) => {
-  console.log(action);
   switch (action.type) {
     case CREATE_TODO:
       return {
+        ...state,
         todos: [...state.todos, action.payload],
       };
     case DELETE_TODO:
       return {
+        ...state,
         todos: state.todos.filter((todo) => todo.id !== action.payload),
       };
     case UPDATE_TODO:
       return {
+        ...state,
         todos: state.todos.map((todo) => {
           if (todo.id !== action.payload) {
+            // todos의리스트와 payload의 id가 같지 않을 때 객체리스트들을 그냥 반환한다.
+            return todo;
+          } else {
+            todo.isDone = !todo.isDone;
+            //todos의 리스트와 payload의 id가 같을 때 그 객체리스트의 isDone을 반대로 값을 바꿔준다.
             return todo;
           }
-          todo.isDone = !todo.isDone;
-          return todo;
+        }),
+      };
+    case READ_TODO:
+      console.log(state.detail);
+      return {
+        ...state,
+        detail: state.todos.filter((item) => {
+          return item.id === action.payload;
         }),
       };
 
