6c89677a98420e0c26527cdbc9f06ee45931746e
minseung-gang <strause1@nate.com>
2022-12-07 17:58:46 +0900

    last

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import store from "./redux/config/configStore";
 import { Provider } from "react-redux";
-
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
